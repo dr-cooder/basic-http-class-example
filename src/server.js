8451a210e88183789201704d24cac45339d1cd0b
Felix Warner (RIT Student) <fgw7468@rit.edu>
2022-09-02 13:43:02 -0400

    added additional pages

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 const http = require('http');
-const fs = require('fs');
+const responses = require('./responses.js');
+console.log(responses);
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
-
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-console.log(index);
 
 const onRequest = (request, response) => {
   console.log(request.url);
 
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write(index);
-
-  response.end();
+  if (request.url==='/client2'){
+    responses.getClient2(request, response);
+  } else if (request.url==='/'){
+    responses.getIndex(request, response);
+  } else {
+    responses.getMessage(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
